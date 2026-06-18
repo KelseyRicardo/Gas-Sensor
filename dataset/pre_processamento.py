2dbfb5d189865865d7aff0fce050e37837e39281
KelseyRicardo <ricardo112.41@gmail.com>
2026-06-18 15:38:17 -0300

    questões

--- dataset/pre_processamento.py
+++ dataset/pre_processamento.py
@@ -1,5 +1,14 @@
 import pandas as pd
-data = pd.read_csv('../dataset/batch1.dat', sep=' ', header=None)
+batch1 = pd.read_csv('../dataset/batch1.dat', sep=' ', header=None)
+batch2 = pd.read_csv('../dataset/batch2.dat', sep=' ', header=None)
+batch3 = pd.read_csv('../dataset/batch3.dat', sep=' ', header=None)
+batch4 = pd.read_csv('../dataset/batch4.dat', sep=' ', header=None)
+batch5 = pd.read_csv('../dataset/batch5.dat', sep=' ', header=None)
+batch6 = pd.read_csv('../dataset/batch6.dat', sep=' ', header=None)
+batch7 = pd.read_csv('../dataset/batch7.dat', sep=' ', header=None)
+batch8 = pd.read_csv('../dataset/batch8.dat', sep=' ', header=None)
+batch9 = pd.read_csv('../dataset/batch9.dat', sep=' ', header=None)
+batch10 = pd.read_csv('../dataset/batch10.dat', sep=' ', header=None)
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 10)
 
@@ -15,8 +24,16 @@ def transformar_data(linhas):
             feature_index, feature_value = linhas[i].split(':')
             features[f"feature_{int(feature_index)}"] = float(feature_value)
 
-    # Return a dictionary with the gas type, concentration, and features
     return {"gas_type": gas_type, "concentration": concentration, **features}
-data = data.apply(transformar_data, axis=1, result_type='expand')
-#print(data)
-print(data['gas_type'].value_counts().sort_index())
+batch1 = batch1.apply(transformar_data, axis=1, result_type='expand')
+batch2 = batch2.apply(transformar_data, axis=1, result_type='expand')
+batch3 = batch3.apply(transformar_data, axis=1, result_type='expand')
+batch4 = batch4.apply(transformar_data, axis=1, result_type='expand')
+batch5 = batch5.apply(transformar_data, axis=1, result_type='expand')
+batch6 = batch6.apply(transformar_data, axis=1, result_type='expand')
+batch7 = batch7.apply(transformar_data, axis=1, result_type='expand')
+batch8 = batch8.apply(transformar_data, axis=1, result_type='expand')
+batch9 = batch9.apply(transformar_data, axis=1, result_type='expand')
+batch10 = batch10.apply(transformar_data, axis=1, result_type='expand')
+#print(batch1)
+print(batch1['gas_type'].value_counts().sort_index())
