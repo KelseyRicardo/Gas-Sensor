3d6452a1755da7f2bced187a9c6432cfaa279dda
KelseyRicardo <ricardo112.41@gmail.com>
2026-06-21 21:41:06 -0300

    import

--- dataset/pre_processamento.py
+++ dataset/pre_processamento.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from sklearn.feature_selection import f_classif
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import numpy as np
 
 batch1 = pd.read_csv('../dataset/batch1.dat', sep=' ', header=None)
@@ -13,14 +13,13 @@ batch7 = pd.read_csv('../dataset/batch7.dat', sep=' ', header=None)
 batch8 = pd.read_csv('../dataset/batch8.dat', sep=' ', header=None)
 batch9 = pd.read_csv('../dataset/batch9.dat', sep=' ', header=None)
 batch10 = pd.read_csv('../dataset/batch10.dat', sep=' ', header=None)
-pd.set_option('display.max_rows', 15)
-pd.set_option('display.max_columns', 15)
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', 20)
 
 def transformar_data(linhas):
     gas, concentracao = linhas[0].split(';')
     concentracao = float(concentracao)
     gas = int(gas)
-
     features = {}
 
     for i in range(1, len(linhas)):
@@ -40,4 +39,44 @@ batch8 = batch8.apply(transformar_data, axis=1, result_type='expand')
 batch9 = batch9.apply(transformar_data, axis=1, result_type='expand')
 batch10 = batch10.apply(transformar_data, axis=1, result_type='expand')
 all_batch = pd.concat([batch1, batch2, batch3, batch4,batch5,batch6,batch7,batch8,batch9,batch10], ignore_index=True)
-#print(batch1)
+#print(all_batch['gas'].value_counts().sort_index())
+all_batch_numbers = all_batch.copy()
+all_batch['gas'] = all_batch['gas'].replace({
+    1.0: 'Etanol',
+    2.0: 'Etileno',
+    3.0: 'Amônia',
+    4.0: 'Acetaldeído',
+    5.0: 'Acetona',
+    6.0: 'Metilbenzeno'
+})
+
+X = all_batch[[f'feature_{i}' for i in range(1,129)]]
+y = all_batch['gas']
+F, p = f_classif(X, y)
+ranking = pd.DataFrame({
+    'feature': X.columns,
+    'F': F
+}).sort_values('F', ascending=False)
+
+scaler = StandardScaler()
+minmax_scaler = MinMaxScaler(feature_range=(0,1))
+
+selecionadas = []
+limite = 0.9
+for feat in ranking['feature']:
+    if len(selecionadas) == 0:
+        selecionadas.append(feat)
+    else:
+        correlacoes = [
+            abs(all_batch[[feat, f]].corr().iloc[0,1])
+            for f in selecionadas
+        ]
+        if max(correlacoes) < limite:
+            selecionadas.append(feat)
+    if len(selecionadas) == 20:
+        break
+
+X = all_batch[selecionadas].values
+X_ss = scaler.fit_transform(X)
+X_mm = minmax_scaler.fit_transform(X)
+y = all_batch_numbers[['gas']]
